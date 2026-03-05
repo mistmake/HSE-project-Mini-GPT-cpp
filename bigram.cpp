26d5fed7e857280b557e43d6adbe18b08c023bce
pikch <ftnova00@gmail.com>
2026-03-05 23:58:57 +0300

    third Milana

--- bigram.cpp
+++ bigram.cpp
@@ -2,8 +2,36 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <optional>
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
 
 constexpr size_t VOCAB_SIZE = 256;
+class FileIOException : public std::runtime_error {
+public:
+    explicit FileIOException(const std::string& path)
+        : std::runtime_error("Cannot read file: " + path) {}
+};
+class DataLoader {
+public:
+    std::optional<std::string> loadText(const std::string& filepath) {
+        std::ifstream file(filepath); //автоматом закрывает файл
+
+        if (!file.is_open()) {
+            throw FileIOException(filepath); //отбрасывает ошибку
+        }
+        std::ostringstream buffer;
+        buffer << file.rdbuf();
+        std::string content = buffer.str();
+
+        if (content.empty()) {
+            return std::nullopt; //файл открыт, но он пустой
+        }
+
+        return content; //ретернит строчку в "бокс"
+    }
+};
 class LanguageModel {
 public:
     virtual ~LanguageModel() { //деструктор, чтобы дети правильно очищали свою память
@@ -11,6 +39,19 @@ public:
     }
     virtual void train(const std::string& text) = 0; //pure virtual functions
     virtual char predictNext(char current) const = 0;
+    std::string generateSentence(char start_char, int length) const {
+        std::string result = "";
+        result += start_char; // Добавляем первую букву
+
+        char current = start_char;
+        for (int i = 0; i < length - 1; ++i) {
+            char next = predictNext(current); // Угадываем следующую
+            if (next == ' ') break; // Останавливаемся, если модель возвращает пробел (не знает что делать)
+            result += next; // Приклеиваем к результату
+            current = next; // Новая буква становится текущей для следующего шага
+        }
+        return result;
+    }
 };
 class BigramModel : public LanguageModel { //класс - наследник
 private:
@@ -19,7 +60,7 @@ private:
 
 public:
     BigramModel() {
-        counts.resize(VOCAB_SIZE, std::vector<int>(VOCAB_SIZE, 0)); //конструктор, который делает сетку размером 256*256, заполненную 0
+        counts.resize(VOCAB_SIZE, std::vector<int>(VOCAB_SIZE, 0)); //конструктор, который делает сетку размером 256*256, зап     олненную 0
     }
     void train(const std::string& text) override { //составляю пары
         if (text.length() < 2) return; //ставлю условие, что мне нужно мин две буквы для пары
@@ -47,3 +88,31 @@ public:
         return best_next_char;
     }
 };
+
+int main() {
+    DataLoader loader;
+    std::string path = "bigram.txt";
+
+    try {
+        std::optional<std::string> dataset = loader.loadText(path);
+
+        if (dataset.has_value()) {
+
+            std::unique_ptr<LanguageModel> my_model = std::make_unique<BigramModel>();
+            my_model->train(dataset.value());
+            std::vector<std::unique_ptr<LanguageModel>> pipeline;
+            pipeline.push_back(std::move(my_model));
+            std::cout << "\nGenerating text starting with 't':\n";
+            std::string output = pipeline[0]->generateSentence('t', 20);
+            std::cout << output << "\n";
+
+        } else {
+            std::cout << "The dataset file is empty.\n";
+        }
+    } catch (const FileIOException& error) {
+        std::cerr << "CRITICAL ERROR: " << error.what() << "\n";
+        std::cerr << "Please make sure you created the 'bigrame.txt' file!\n";
+    }
+
+    return 0;
+}
